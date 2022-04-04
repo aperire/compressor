db166f88dbebfdd753687baa3b5e49273114fdb3
s355park <96969248+s355park@users.noreply.github.com>
2022-04-04 12:02:59 -0400

    Add files via upload

--- group_constant_finder.py
+++ group_constant_finder.py
@@ -8,23 +8,23 @@ def group_constant_finder(bn_str : str, lowest_range : int, highest_range: int)-
     #key:group_constant
     #value:compressability
     compressability=dict()
-    for group_constant in range(lowest_range,highest_range):
+    for group_constant in range(lowest_range,highest_range+1):
         #probability of num of keys
         key_prob=dict()
         #number of groups, including leftover
         group_num=int(len(bn_str)/group_constant)+1
         #python can't handle large number division, and since all cases have same total cases, 
         #probability will be substituted with number of cases for specific key_variety
-        for key_variety in range(1,2**group_constant):
+        for key_variety in range(1,2**group_constant+1):
             key_prob[key_variety]=(math.comb(2**group_constant,key_variety))*(key_variety**group_num-(key_variety-1)**group_num)
         key_prob=dict(sorted(key_prob.items(), key=lambda group: group[1], reverse=True))
         highest_probability_key=list(key_prob.keys())[0]
         compressability[group_constant]=math.log2(highest_probability_key)*(group_num+highest_probability_key)+group_constant*highest_probability_key
     compressability=dict(sorted(compressability.items(), key=lambda group: group[1], reverse=False))
+    print(compressability)
     if(highest_range-lowest_range+1)<5:
         return list(compressability.keys())[:highest_range-lowest_range+1]
     return list(compressability.keys())[:5]
 
 
-# print(group_constant_finder("10101010100101111010001010100010111101010100101010101010010111101010100011010101010100101010110101010101011",2,3))
 
